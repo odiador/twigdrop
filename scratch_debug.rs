5d1d2209c2ff352a39778f1930b520eaa6477488
Juan Manuel Amador Roa <arroa03@gmail.com>
2026-06-01 17:38:20 -0500

    chore: include debug files in commit to clean working tree

--- scratch_debug.rs
+++ scratch_debug.rs
@@ -1,6 +1,22 @@
 fn main() {
-    let track_str = "[behind 13]";
-    if let Some(b) = track_str.split("behind ").nth(1).and_then(|s| s.split(|c: char| !c.is_numeric()).next()) {
-        println!("behind: {}", b.parse::<usize>().unwrap_or(0));
+    let output = "
+* <|ce2324a|2026-05-31|Juan Manuel Amador Roa|Refactor Config
+|\\  
+| * <|c7e61dc|2026-05-31|Juan|Refactor mouse
+* | <|917b2ea|2026-05-31|Juan|Improve code
+|/  
+* <|e2cef2c|2026-05-24|Juan|Simplify
+";
+    for line in output.lines().filter(|l| !l.is_empty()) {
+        if let Some(idx) = line.find("<|") {
+            let graph = &line[..idx];
+            let rest = &line[idx + 2..];
+            let parts: Vec<&str> = rest.splitn(4, '|').collect();
+            if parts.len() == 4 {
+                println!("Graph: '{}', Hash: {}, Date: {}, Author: {}, Msg: {}", graph, parts[0], parts[1], parts[2], parts[3]);
+            }
+        } else {
+            println!("Graph only: '{}'", line);
+        }
     }
 }
